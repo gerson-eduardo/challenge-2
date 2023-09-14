f622882bfb741f5849ab760adb5fc5636d75ee0b
gerson <gerson.sousa.pb@compasso.com.br>
2023-09-14 13:38:48 -0300

    feat: created the base project structure

---
+++ ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/MsAuthAndAutoApplication.java
@@ -0,0 +1,13 @@
+package com.springmasters.msauthandauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthAndAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthAndAutoApplication.class, args);
+	}
+
+}
