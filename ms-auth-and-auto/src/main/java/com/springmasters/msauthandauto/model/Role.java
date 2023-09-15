4813a5d117a27122c087efa0cafa1e1bb4fb310f
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-09-15 13:16:51 -0300

    feat: first model of role of user

---
+++ ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/model/Role.java
@@ -0,0 +1,62 @@
+package com.springmasters.msauthandauto.model;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer idRole;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;   
+    
+    @ManyToOne
+    @JoinColumn(name = "microservice_id")
+    private Microservice microservice;
+
+    @Enumerated(EnumType.STRING)
+    private userRole roleUser;
+
+    public enum userRole{
+     Admin,
+     User   
+    }
+
+    public User getIdUSer() {
+        return this.idUSer;
+    }                                                                                                           
+
+    public void setIdUSer(User idUSer) {
+        this.user = user;
+    }
+
+    public Microservice getMicrosservice() {
+        return this.microservice;
+    }
+
+    public void setMicrosservice(Microservice microsservice) {
+        this.microservice = microsservice;
+    }
+
+    public String getRoleUser() {
+        return this.roleUser;
+    }
+
+    public void setRoleUser(String roleUser) {
+        this.roleUser = roleUser;
+    }
+}
