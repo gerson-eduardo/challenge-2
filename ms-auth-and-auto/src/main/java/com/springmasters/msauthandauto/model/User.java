d28b98925e21dded59d022870f605502af3ea018
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-09-17 10:53:29 -0300

    refactor: refactor user model and microservice repository

--- ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/model/User.java
+++ ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/model/User.java
@@ -1,18 +1,28 @@
 package com.springmasters.msauthandauto.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "user_id")
     private int id;
     private String name;
     private String email;
     private String password;
+    
+    @OneToMany
+    private List<Role> userRoles;
 
     public User(){
     }
--- ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/repository/MicroserviceRepository.java
+++ ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/repository/MicroserviceRepository.java
@@ -2,6 +2,8 @@ package com.springmasters.msauthandauto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MicroserviceRepository extends JpaRepository<MicroserviceRepository, Integer>{
+import com.springmasters.msauthandauto.model.Microservice;
+
+public interface MicroserviceRepository extends JpaRepository<Microservice, Integer>{
     
 }
