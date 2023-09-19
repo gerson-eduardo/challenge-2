f2408902f9eac3f48898f19e98771f7f46b0208f
Izaias Valentim <izaias451@gmail.com>
2023-09-19 15:48:44 -0300

    build/inject the feign client.

--- b/src/main/java/br/com/ms/b/controller/msBController.java
+++ b/src/main/java/br/com/ms/b/controller/msBController.java
@@ -1,5 +1,7 @@
 package br.com.ms.b.controller;
 
+import br.com.ms.b.feignClient.MsAuthAndAutoFeign;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "api/scholar/", consumes = MediaType.APPLICATION_JSON_VALUE)
 public class msBController {
+    private final MsAuthAndAutoFeign msAuthAndAutoFeign;
 
+    @Autowired
+    public msBController(MsAuthAndAutoFeign msAuthAndAutoFeign) {
+        this.msAuthAndAutoFeign = msAuthAndAutoFeign;
+    }
 }
