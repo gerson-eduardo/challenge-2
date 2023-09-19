19bb957cae57079f83a33588e71ce04138085057
Izaias Valentim <izaias451@gmail.com>
2023-09-19 15:47:33 -0300

    feat/added the RequestMapping.

--- b/src/main/java/br/com/ms/b/controller/msBController.java
+++ b/src/main/java/br/com/ms/b/controller/msBController.java
@@ -1,7 +1,11 @@
 package br.com.ms.b.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "api/scholar/", consumes = MediaType.APPLICATION_JSON_VALUE)
 public class msBController {
+
 }
