80753b1561f2548db5f1effc79fec2d3d7f9224c
Izaias Valentim <izaias451@gmail.com>
2023-09-19 15:31:07 -0300

    feat/added the main project of ms-scholar-b

---
+++ b/src/main/java/br/com/ms/b/BApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BApplication.class, args);
+	}
+
+}
