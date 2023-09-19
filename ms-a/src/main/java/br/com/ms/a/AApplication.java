7e5e2b6c00d9f44a1aa9a12e94a6d11d163c4605
Eduard0-GE <gersonabes@gmail.com>
2023-09-18 22:15:10 -0300

    feat: created the base project structure

---
+++ ms-a/src/main/java/br/com/ms/a/AApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AApplication.class, args);
+	}
+
+}
