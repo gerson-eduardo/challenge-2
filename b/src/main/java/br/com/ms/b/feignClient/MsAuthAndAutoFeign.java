7bf0d6ea69567f0bab234c3c44716cb0e2cbf5a6
Izaias Valentim <izaias451@gmail.com>
2023-09-19 15:40:41 -0300

    refactor/configure the FeignClient

--- b/src/main/java/br/com/ms/b/feignClient/MsAuthAndAutoFeign.java
+++ b/src/main/java/br/com/ms/b/feignClient/MsAuthAndAutoFeign.java
@@ -1,4 +1,7 @@
 package br.com.ms.b.feignClient;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "ms-auth-and-auto", url = "http://localhost:8080/ms-auth-and-auto/")
 public interface MsAuthAndAutoFeign {
 }
