ec48efcad1d181c132ecdd36e50a9835bdcd555a
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-09-20 07:47:35 -0300

    feat: new function search e create roleController

---
+++ ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/controller/RoleController.java
@@ -0,0 +1,28 @@
+package com.springmasters.msauthandauto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springmasters.msauthandauto.model.Role.userRole;
+import com.springmasters.msauthandauto.service.RoleService;
+
+@RestController
+@RequestMapping(value = "api/roles/", consumes = MediaType.APPLICATION_JSON_VALUE)
+public class RoleController {
+    private final RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService){
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/{id_user}/microsservice/{id_microsservice}")
+    public ResponseEntity<userRole>getRoleByUserAndMicrosservice(@PathVariable Integer id_user,@PathVariable Integer id_microsservice){
+        return roleService.findByUserAndMicrosservice(id_user, id_microsservice);
+    }
+}
