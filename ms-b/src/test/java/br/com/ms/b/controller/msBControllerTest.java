13b5d4bdecd81c7368b8cdd12b95e452e30137d2
Izaias Valentim <izaias451@gmail.com>
2023-09-20 15:06:41 -0300

    feat/test for the getAllUser method.

--- ms-b/src/test/java/br/com/ms/b/controller/msBControllerTest.java
+++ ms-b/src/test/java/br/com/ms/b/controller/msBControllerTest.java
@@ -1,13 +1,42 @@
 package br.com.ms.b.controller;
 
+import br.com.ms.b.feignClient.MsAuthAndAutoFeign;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
-class msBControllerTest {
+class MsBControllerTest {
+    @InjectMocks
+    private MsBController msBController;
+    @Mock
+    private MsAuthAndAutoFeign msAuthAndAutoFeign;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void getAllUser() {
+        // Simula o comportamento do feign client.
+        when(msAuthAndAutoFeign.getAllByIdUserIdMicrosservce(anyInt(), anyInt()))
+                .thenReturn(new ResponseEntity<>("some-body", HttpStatus.OK));
+
+        ResponseEntity<Object> response = msBController.getAllUser(1, 1);
+
+        // Verifica se o método do feign client foi chamado com os argumentos corretos.
+        verify(msAuthAndAutoFeign,
+                times(1)).getAllByIdUserIdMicrosservce(1, 1);
+
+        // Verifica se a resposta HTTP é a esperada.
+        assert (response.getStatusCode()).equals(HttpStatus.OK);
     }
 
     @Test
