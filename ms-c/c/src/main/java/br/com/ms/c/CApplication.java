7c3076e4e5b5bc26d4ea95c428d4c948ce223919
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-20 14:01:00 -0300

    chore: Configure OpenFeign client and create controllers

--- ms-c/c/src/main/java/br/com/ms/c/CApplication.java
+++ ms-c/c/src/main/java/br/com/ms/c/CApplication.java
@@ -2,10 +2,11 @@ package br.com.ms.c;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CApplication.class, args);
 	}
