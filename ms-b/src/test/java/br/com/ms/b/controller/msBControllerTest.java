c3b76cb54ca918fd5068f052432f1be86bfb806c
Izaias Valentim <izaias451@gmail.com>
2023-09-20 14:52:59 -0300

    feature/add the file of tests.

---
+++ ms-b/src/test/java/br/com/ms/b/controller/msBControllerTest.java
@@ -0,0 +1,16 @@
+package br.com.ms.b.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class msBControllerTest {
+
+    @Test
+    void getAllUser() {
+    }
+
+    @Test
+    void getAllByMicrosservice() {
+    }
+}
