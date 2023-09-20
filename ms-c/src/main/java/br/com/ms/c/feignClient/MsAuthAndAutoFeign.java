f2090a6ecce86d81213fdd422d1f1927fbc231f9
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-20 15:29:07 -0300

    refactor: changed path of the feign interface

--- ms-c/src/main/java/br/com/ms/c/feignClient/MsAuthAndAutoFeign.java
+++ ms-c/src/main/java/br/com/ms/c/feignClient/MsAuthAndAutoFeign.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "ms-auth-and-auto", url = "http://localhost:8080/ms-auth-and-auto/")
 public interface MsAuthAndAutoFeign {
-    @GetMapping(value = "api/users/{id_user}/microsservice/{id_microsservice}",
+    @GetMapping(value = "api/roles/{id_user}/microsservice/{id_microsservice}",
             consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Object> getAllByIdUserIdMicrosservce(@PathVariable Integer id_user,
                                                         @PathVariable Integer id_microsservice);
 
-    @GetMapping(value = "api/users/{id_microsservice}",
+    @GetMapping(value = "api/microsservices/{id_microsservice}",
             consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Object> getByIdMicrosservice(@PathVariable Integer id_microsservice);
 }
