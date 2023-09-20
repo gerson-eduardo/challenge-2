6cba66b4ce81f474a97c065efd7be494d1d0c717
Eduard0-GE <gersonabes@gmail.com>
2023-09-20 17:33:14 -0300

    test: created the test file for controller and feignclient

---
+++ ms-a/src/test/java/br/com/ms/a/feingnclient/CentralFeignTest.java
@@ -0,0 +1,16 @@
+package br.com.ms.a.feingnclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CentralFeignTest {
+
+    @Test
+    void getByIdMicrosservice() {
+    }
+
+    @Test
+    void getAllByIdUserIdMicrosservce() {
+    }
+}
