1c6dbb261b4e2a00044cca4386813d8a95571b2a
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-09-20 15:08:40 -0300

    feat: create new request in RoleController

--- ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/controller/RoleController.java
+++ ms-auth-and-auto/src/main/java/com/springmasters/msauthandauto/controller/RoleController.java
@@ -1,5 +1,7 @@
 package com.springmasters.msauthandauto.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springmasters.msauthandauto.DTO.RoleDTO;
+import com.springmasters.msauthandauto.DTO.RoleWithUserDTO;
 import com.springmasters.msauthandauto.model.Role.userRole;
 import com.springmasters.msauthandauto.service.RoleService;
 
@@ -25,5 +28,10 @@ public class RoleController {
     @GetMapping("/{id_user}/microsservice/{id_microsservice}")
     public ResponseEntity<RoleDTO>getRoleByUserAndMicrosservice(@PathVariable Integer id_user,@PathVariable Integer id_microsservice){
         return roleService.findByUserAndMicrosservice(id_user, id_microsservice);
+    }
+
+    @GetMapping("/{id_microsservice}")
+    public ResponseEntity<List<RoleWithUserDTO>> getRolesByMicrosservice(@PathVariable Integer id_microsservice){
+        return roleService.findByMicrosservice(id_microsservice);
     }
 }
