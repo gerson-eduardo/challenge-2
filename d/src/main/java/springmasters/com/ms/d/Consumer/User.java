65d81b2bcdb4b38582dede133756c30d545d5c77
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-09-20 10:46:40 -0300

    feat: create user for consume api

---
+++ d/src/main/java/springmasters/com/ms/d/Consumer/User.java
@@ -0,0 +1,83 @@
+package springmasters.com.ms.d.Consumer;
+import java.util.Objects;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User id(Integer id) {
+        setId(id);
+        return this;
+    }
+
+    public User name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public User email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", email='" + getEmail() + "'" +
+            "}";
+    }
+    
+}
