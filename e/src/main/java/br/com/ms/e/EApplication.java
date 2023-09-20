bdcba2bffd8d9aa65a3dd91802707be5472d4774
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-09-20 13:57:17 -0300

    feat: added main project ms-scholar-e

---
+++ e/src/main/java/br/com/ms/e/EApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EApplication.class, args);
+	}
+
+}
