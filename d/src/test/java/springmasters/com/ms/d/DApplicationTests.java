c5751817ac5890acb47e72ab228bceb37fc9ccf3
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-09-19 23:06:15 -0300

    config:application propierties config

---
+++ d/src/test/java/springmasters/com/ms/d/DApplicationTests.java
@@ -0,0 +1,13 @@
+package springmasters.com.ms.d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
