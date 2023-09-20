7abb4510aa9c2e8774d7f7ba8aabb0762a19b674
Gabriela <gabriela.bvieira@hotmail.com>
2023-09-20 01:01:59 -0300

    feat: ms-c package created

---
+++ ms-c/c/src/main/java/br/com/ms/c/CApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CApplication.class, args);
+	}
+
+}
